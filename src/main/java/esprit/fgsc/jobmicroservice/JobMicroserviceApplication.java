6dfbab6ea629aff9ab20029c14fbc0c9b8db3468
ghadakhedri1 <ghada.khedri1@esprit.tn>
2021-10-17 21:58:25 +0100

    Rectifications

--- src/main/java/esprit/fgsc/jobmicroservice/JobMicroserviceApplication.java
+++ src/main/java/esprit/fgsc/jobmicroservice/JobMicroserviceApplication.java
@@ -2,8 +2,10 @@ package esprit.fgsc.jobmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JobMicroserviceApplication {
 
 	public static void main(String[] args) {
